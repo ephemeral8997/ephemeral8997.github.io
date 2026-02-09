aa4ccbd920ca9bed656f98cef9b1005deb247f51
ephemeral8997 <mazen001.ahmed001@gmail.com>
2026-02-09 20:32:10 +0200

    Make links in one area

---
+++ config.js
@@ -0,0 +1,38 @@
+// config.js
+const SITE_CONFIG = {
+    profile: {
+        username: "ephemeral8997",
+        pfp: "pfp.png",
+        github: "https://github.com/ephemeral8997",
+    },
+    socials: [
+        {
+            name: "GitHub",
+            url: "https://github.com/ephemeral8997",
+            icon: "📦",
+            description: "Code repositories and open source projects",
+            handle: "github.com/ephemeral8997"
+        },
+        {
+            name: "Fandom",
+            url: "https://community.fandom.com/f/u/53882193",
+            icon: "🌟",
+            description: "Community engagement and discussions",
+            handle: "community.fandom.com/u/53882193"
+        },
+        {
+            name: "ElevenForum",
+            url: "https://www.elevenforum.com/members/ephemeral8997.52407/about",
+            icon: "💬",
+            description: "Tech discussions and support",
+            handle: "elevenforum.com/members/ephemeral8997"
+        },
+        {
+            name: "Wikimedia",
+            url: "https://commons.wikimedia.org/wiki/User_talk:Ephemeral8997",
+            icon: "📚",
+            description: "Media contributions and discussions",
+            handle: "commons.wikimedia.org/wiki/User_talk:Ephemeral8997"
+        }
+    ]
+};
